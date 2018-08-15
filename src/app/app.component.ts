c63587ff428a098dea9a1a814d373e31450ee8e0
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-08-15 15:52:23 -0700

    finished weekend homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentFocus: string = "Angular Homework";
+  currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-
   selectedTask = null;
 
   masterTaskList: Task[] = [
-   new Task('Finish weekend Angular homework for Epicodus course', 3),
-   new Task('Begin brainstorming possible JavaScript group projects', 2),
-   new Task('Add README file to last few Angular repos on GitHub', 2)
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
-  finishedEditing(){
+
+  finishedEditing() {
     this.selectedTask = null;
+  }
+  addTask(newTask: Task) {
+    this.masterTaskList.push(newTask);
   }
 }
--- src/app/new-task/new-task.component.spec.ts
+++ src/app/new-task/new-task.component.spec.ts
@@ -1,15 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { TaskListComponent } from './task-list.component';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
-
-  var filterByCompleteness: string = "incompleteTasks";
-
-  // onChange(optionFromMenu) {
-  //   this.filterByCompleteness = optionFromMenu;
-  // }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,7 +22,4 @@ describe('TaskListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
- //  toggleDone(clickedTask: Task, setCompleteness: boolean) {
- //   clickedTask.done = setCompleteness;
- // }
 });
